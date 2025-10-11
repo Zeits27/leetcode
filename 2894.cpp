a6bb565d1380ebef8a91f67d3c017ff12a612070
Laurentius Nicholas <email.nick2702@gmail.com>
2025-10-11 23:35:34 +0700

    2894

---
+++ 2894.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int differenceOfSums(int n, int m)
+    {
+        int ans = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            if (i >= m && i % m == 0)
+            {
+                ans -= i;
+            }
+            else
+            {
+                ans += i;
+            }
+        }
+        return ans;
+    }
+};
